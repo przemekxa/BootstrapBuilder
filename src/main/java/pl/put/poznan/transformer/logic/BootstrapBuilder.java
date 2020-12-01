f97fc7ecf9987eee753d5c6fd58bf7d974b5b2dd
piter104 <piotr.derenowski@gmail.com>
2020-12-01 09:55:16 +0100

    project name refractor

--- src/main/java/pl/put/poznan/transformer/logic/BootstrapBuilder.java
+++ src/main/java/pl/put/poznan/transformer/logic/BootstrapBuilder.java
@@ -3,15 +3,15 @@ package pl.put.poznan.transformer.logic;
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
-public class TextTransformer {
+public class BootstrapBuilder {
 
     private final String[] transforms;
 
-    public TextTransformer(String[] transforms){
+    public BootstrapBuilder(String[] transforms) {
         this.transforms = transforms;
     }
 
-    public String transform(String text){
+    public String transform(String text) {
         // of course, normally it would do something based on the transforms
         return text.toUpperCase();
     }
