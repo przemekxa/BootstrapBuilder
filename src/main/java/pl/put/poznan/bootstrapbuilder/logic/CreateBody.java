b325379d96eceb568f275311e6bc79511a542220
Feliks Czart <czart78@gmail.com>
2020-12-07 19:27:28 +0100

    meta tags working

--- src/main/java/pl/put/poznan/bootstrapbuilder/logic/CreateBody.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/logic/CreateBody.java
@@ -1,18 +1,30 @@
 package pl.put.poznan.bootstrapbuilder.logic;
 
+import pl.put.poznan.bootstrapbuilder.rest.HeaderType;
 import pl.put.poznan.bootstrapbuilder.rest.Request;
 
+/**
+ * This is ConcreteBuilder class in builder pattern.
+ */
 public class CreateBody implements Builder {
     private String body = "";
 
     @Override
     public void build(Request request) {
+        String text = "";
+        HeaderType headerType = request.getHeader();
+
+        if (headerType != HeaderType.NONE) {
+            text = "class='position-" + headerType.toString().toLowerCase() + "'";
+        }
         body =
                 "<body>\n" +
-                        "   <header>\n" +
-                        "       <h>Strona główna</h>\n" +
-                        "   </header>\n" +
-                        "   <p> jakis opis </p>\n ";
+                        "<header " + text + ">\n" +
+                        "    <h>Strona główna</h>\n" +
+                        "</header>\n" +
+                        "<main>\n" +
+                        "    <p> jakis opis </p>\n" +
+                        "</main>\n";
 
     }
 
--- src/main/java/pl/put/poznan/bootstrapbuilder/logic/CreateHeader.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/logic/CreateHeader.java
@@ -2,6 +2,9 @@ package pl.put.poznan.bootstrapbuilder.logic;
 
 import pl.put.poznan.bootstrapbuilder.rest.Request;
 
+/**
+ * This is ConcreteBuilder class in builder pattern.
+ */
 public class CreateHeader implements Builder {
     private String header = "";
 
@@ -11,9 +14,13 @@ public class CreateHeader implements Builder {
                 "<!DOCTYPE html>\n" +
                         "<html lang=\"en\">\n" +
                         "<head>\n" +
-                        "    <meta charset=\"UTF-8\">\n" +
-                        "    <title>Title</title>\n" +
+                        "    <!-- Required meta tags -->\n" +
+                        "    <meta charset=\"utf-8\">\n" +
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n" +
                         "\n" +
+                        "    <!-- Bootstrap CSS -->\n" +
+                        "    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n" +
+                        "    <title>Title</title>\n" +
                         "    <style>\n" +
                         "        body {\n" +
                         "            background-image: url(\"http://www.cs.put.poznan.pl/wcomplak/IMG/smash.gif\");\n" +
