b360a66309e16e33cbbe696f1f691a45f741d04a
Feliks Czart <czart78@gmail.com>
2020-12-09 16:44:38 +0100

    Documentation

--- src/main/java/pl/put/poznan/bootstrapbuilder/app/BootstrapBuilderApplication.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/app/BootstrapBuilderApplication.java
@@ -3,7 +3,9 @@ package pl.put.poznan.bootstrapbuilder.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * This is the Spring Boot runnable class which deals with running the whole application.
+ */
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.bootstrapbuilder.rest"})
 public class BootstrapBuilderApplication {
 
