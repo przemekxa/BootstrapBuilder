c221a7ec777724af5bcf62c827ba9e11aede64a1
Przemek Ambroży <przemek19986@gmail.com>
2020-12-14 14:05:16 +0100

    Fix

--- src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderController.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/rest/BootstrapBuilderController.java
@@ -61,9 +61,6 @@ public class BootstrapBuilderController {
                 .setFooter(request.getFooter())
                 .build();
 
-
-        template.trim();
-
         return template;
     }
 }
