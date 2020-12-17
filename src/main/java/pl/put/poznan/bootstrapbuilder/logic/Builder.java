319caa1264557fff0ca17ff45791d852679da6ee
Przemek Ambroży <przemek19986@gmail.com>
2020-12-17 22:41:56 +0100

    Add methods to Builder interface

--- src/main/java/pl/put/poznan/bootstrapbuilder/logic/Builder.java
+++ src/main/java/pl/put/poznan/bootstrapbuilder/logic/Builder.java
@@ -1,5 +1,9 @@
 package pl.put.poznan.bootstrapbuilder.logic;
 
+import pl.put.poznan.bootstrapbuilder.rest.HeaderType;
+
 public interface Builder {
     public String build();
+    public Builder setHeader(HeaderType type);
+    public Builder setFooter(boolean footer);
 }
